42d2239f73f17ccca2f65c2732e2e76b0c06d9f9
mark <mark@totoro2-2.local>
2025-05-25 09:46:37 -0400

    do things more cleanly - just pull version dirs in when getting versions - make sure index.html is re-created each time - look at all versions

--- scripts/create-index.sh
+++ scripts/create-index.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-touch dist/index.html
-# redirect the output of this script to file
-exec 1>>dist/index.html
+# redirect the output of this script to file (overwrite it)
+exec 1>dist/index.html
 
 echo '<h1>Previous versions of MFTC</h1>'
 echo '<p>Only 1 version can be installed at a time. Be sure to uninstall previous versions to avoid unexpected behavior.</p>'
